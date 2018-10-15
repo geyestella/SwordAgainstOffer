ddb53d32c3f565009f38dfa95cb2f8192a14a49c
geyestella <43089824+geyestella@users.noreply.github.com>
2018-10-15 17:56:39 +0800

    Update test1.java

---
+++ test1.java
@@ -0,0 +1,11 @@
+public class Solution {
+	public static boolean Find(int target, int [][] array) {
+	    for(int i=0;i<array.length;i++){
+	        for(int j=0;j<array[i].length;j++){
+	            if (array[i][j]==target)
+	                return true;
+	        }
+	    }
+	        return false;
+	        }
+}
